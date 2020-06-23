2f11bb0b568396a64a51abd9467f1d334977ed85
Raiosilva <raimundopi025@gmail.com>
2020-06-23 20:29:59 -0300

    adding setup initial

---
+++ src/main/java/com/rtc/service/RtcApplication.java
@@ -0,0 +1,13 @@
+package com.rtc.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtc/service/RtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtc.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
